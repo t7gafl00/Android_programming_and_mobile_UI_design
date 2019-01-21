f178c338eadb4901f4b6af3fcdfb8475423d1d0f
t7gafl00 <t7gafl00@students.oamk.fi>
2019-01-21 11:00:49 +0200

    Added persistant storage to Lab_2_2

--- Lab_2/Lab_2_2/app/src/main/java/com/example/lab_2_2/MainActivity.java
+++ Lab_2/Lab_2_2/app/src/main/java/com/example/lab_2_2/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.lab_2_2;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import static android.app.PendingIntent.getActivity;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int count = 0;
+    private int count;
     private Button gameButton;
+
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        sharedPref = getSharedPreferences("", Context.MODE_PRIVATE);
+        count = sharedPref.getInt("countKey", 0);
+        editor = sharedPref.edit();
+
         gameButton = new Button(this);
         gameButton.setOnClickListener(this);
-        gameButton.setText("Hello, I'm the button");
+        //gameButton.setText("Hello, I'm the button");
+        gameButton.setText(String.format("You have pressed the button %d times.", count));
         setContentView(gameButton);
     }
 
@@ -24,5 +36,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         count++;
         gameButton.setText(String.format("You have pressed the button %d times.", count));
+
+        editor.putInt("countKey", count);
+        editor.apply();
     }
 }
