0450db9e5f40f3e2b83b07989041cb7bd136cbf6
t7gafl00 <t7gafl00@students.oamk.fi>
2019-02-06 10:23:05 +0200

    Done Lab_4_2, problem with TextToSpeech delay on first use

--- Lab_4/Lab_4_2/app/src/main/java/com/example/myapplicationlab_4_2/RunProgramTimerActivity.java
+++ Lab_4/Lab_4_2/app/src/main/java/com/example/myapplicationlab_4_2/RunProgramTimerActivity.java
@@ -2,11 +2,13 @@ package com.example.myapplicationlab_4_2;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import model.WorkoutPartBase;
 
@@ -19,6 +21,8 @@ public class RunProgramTimerActivity extends AppCompatActivity {
 
     int total_duration = 0;
 
+    TextToSpeech ttobj;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +31,11 @@ public class RunProgramTimerActivity extends AppCompatActivity {
         activity_type_TextView = findViewById(R.id.activity_type_TextView);
         activity_duration_TextView = findViewById(R.id.activity_duration_TextView);
 
+        // Get activities from MainActivity
         Intent intent = getIntent();
-
-        // Get duration of the timer set in MainActivity
         workouts_array_list = (ArrayList<WorkoutPartBase>) intent.getSerializableExtra("ACTIVITIES_LIST");
 
         /*** Compute total duration ***/
-
         for(int i = 0; i < workouts_array_list.size(); i++)
         {
             total_duration += workouts_array_list.get(i).getDuration() + 1;
@@ -44,12 +46,24 @@ public class RunProgramTimerActivity extends AppCompatActivity {
             int i = 0;
             int time_left = 0;
             boolean move_on_to_next_activity = true;
+            String message_to_say;
 
             public void onTick(long millisUntilFinished) {
 
-                if (move_on_to_next_activity == true) {
+                if (move_on_to_next_activity) {
                     time_left = workouts_array_list.get(i).getDuration();
                     move_on_to_next_activity = false;
+
+                    message_to_say = workouts_array_list.get(i).getActivity_type() + Integer.toString(workouts_array_list.get(i).getDuration()) + "seconds";
+
+                    ttobj = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                        @Override
+                        public void onInit(int status) {
+                            ttobj.setLanguage(Locale.US);
+                            //ttobj.speak("", TextToSpeech.QUEUE_FLUSH, null);
+                            ttobj.speak(message_to_say, TextToSpeech.QUEUE_FLUSH, null);
+                        }
+                    });
                 }
 
                 activity_type_TextView.setText(workouts_array_list.get(i).getActivity_type());
